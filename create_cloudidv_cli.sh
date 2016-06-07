9ead57a68836045fd005bfc61250505ec576cdb8
Ward Fisher <wfisher@ucar.edu>
2016-06-07 14:04:07 -0600

    Created a cloudIDV script to start a bunch of cloudidv instances.

--- create_cloudidv_cli.sh
+++ create_cloudidv_cli.sh
@@ -23,31 +23,117 @@ set -e
 ########################################
 
 ###
+# Spawn an instance
+#
+# Input: name, port, password
+###
+spawnidv() {
+
+    HNAME=$1
+    PORT=$2
+    PWORD=$3
+
+    ##
+    # Remove extant named machine if it exists.
+    ##
+    echo "Checking if $HNAME exists, removing if so."
+    set +e
+    if [ $(docker ps -a | grep $HNAME | wc -l | tr -s " " | cut -d " " -f 2 ) -gt 0 ]; then
+        docker rm $HNAME
+    fi
+    set -e
+    ###
+    # Make sure data volume is there.
+    ###
+    DVOL=$(pwd)/datavols/$HNAME
+    if [ ! -d "$DVOL" ]; then
+        mkdir -p $DVOL
+    fi
+
+    echo "Processing $HNAME $PORT $PWORD"
+
+    docker run -d -it --hostname $HNAME --name $HNAME -p $PORT:6080 -e PASS=$PWORD -v $DVOL:/home/stream/.unidata unidata/cloudidv
+
+
+}
+
+###
 # Print out the help dialog.
 ###
 dohelp() {
 
     echo -e ""
-    echo "Usage: $0 [instance name] [port] [password]"
+    echo "Usage: $0 [instance name] [port] [password] -f <filename>"
     echo ""
     echo -e "  * [instance name]:\tThis will also be used for the hostname"
     echo -e "                    \tand the data directory mapping."
     echo -e "  * [port]:\t\tThe external port to expose for connections."
     echo -e "  * [password]:\t\tThe session must be password protected."
-    echo -e ""
+     echo -e "  * <filename>:\t\tYou may alternatively construct a SPACE-delimited file containing this information and pass it with the -f switch."
+   echo -e ""
     echo -e "Example:"
     echo -e ""
     echo -e "\t$ $0 test1 6080 abcd1234"
+    echo -e "\t$ $0 -f machines.txt"
     echo -e ""
 }
 
-if [ $# -ne 3 ]; then
-    echo ""
-    echo "Wrong number of arguments!"
-    echo ""
-    dohelp
-    exit 1
+INFILE=""
+
+while getopts "f:" o; do
+    case "${o}" in
+        f)
+            INFILE=${OPTARG}
+            ;;
+        *)
+            dohelp
+            exit 0
+            ;;
+    esac
+done
+
+
+
+
+if [ "x$INFILE" == "x" ]; then
+
+    if [ $# -ne 3 ]; then
+        echo ""
+        echo "Wrong number of arguments!"
+        echo ""
+        dohelp
+        exit 1
+    fi
+
+    HNAME=$1
+    PORT=$2
+    PASS=$3
+
+    ##
+    # Run the instance detached
+    ##
+    spawnidv $HNAME $PORT $PASS
+
+
+else # Reading in from a file
+
+    if [ ! -f $INFILE ]; then
+        echo "Error reading $INFILE."
+        exit 1
+    fi
+
+    echo "Reading configuration in from file: $INFILE"
+
+    while read HNAME PORT PASS
+    do
+
+        spawnidv $HNAME $PORT $PASS
+        sleep 1
+
+    done < $INFILE
+
 fi
+
 
 
 
