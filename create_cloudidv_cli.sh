60595bc92ccaa8cb0e9ecc9feea6a16bb3492e2f
Ward Fisher <wfisher@ucar.edu>
2016-06-07 11:28:26 -0600

    Added a CLI script for easily spawning docker instances.

---
+++ create_cloudidv_cli.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+########################################
+# Copyright 2016 Unidata
+#
+# This is a utility script for creating a
+# CloudIDV instance from the command line.
+# It doesn't do anything you can't do with
+# the docker-cli, but it is simplified.
+########################################
+
+########################################
+# Input:
+#
+# HostName/InstanceName (these will be the same)
+# Password to use for authentication
+# Port to open on.
+#
+# The script will mount the .unidata directory from
+# the docker container into $(pwd)/docker-volumes/$docker-hostname
+########################################
+
+###
+# Print out the help dialog.
+###
+dohelp() {
+
+    echo -e ""
+    echo "Usage: $0 [instance name] [port] [password]"
+    echo -e "* [instance name]:\tThis will also be used for the hostname"
+    echo -e "                  \tand the data directory mapping."
+    echo -e "* [port]:\t\tThe external port to expose for connections."
+    echo -e "* [password]:\t\tThe session must be password protected."
+    echo -e ""
+    echo -e ""
+}
+
+if [ $# -ne 3 ]; then
+    dohelp
+    exit 1
+fi
+
+
+
+echo "Finished"
